e5581a004ca7ba9370d85bbe3bde95f2ab0e07d6
Daniel Bernal <yo@danielbernal.co>
2020-07-30 18:34:46 -0500

    Initial Commit

---
+++ Redux-Architecture-Part1/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Redux-Architecture-Part1
+//
+//  Created by Daniel Bernal on 7/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Redux-Architecture-Part1/Redux_Architecture_Part1App.swift
@@ -0,0 +1,17 @@
+//
+//  Redux_Architecture_Part1App.swift
+//  Redux-Architecture-Part1
+//
+//  Created by Daniel Bernal on 7/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Redux_Architecture_Part1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
